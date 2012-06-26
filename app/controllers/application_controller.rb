912383fb9ed220067668f598833c840daaa19697
Jay Buffington <jay.buffington@mylookout.com>
2012-06-25 18:52:37 -0700

    500 errors should be in json when accept is json
    When dnsdb throws an exception it causes a 500
    error to be returned to the user.  If the accept
    header is application/json then the error should
    be formatted as json, not HTML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from Exception, :with => :render_500
+
+  private
+  def render_500(exception)
+    @error = exception
+    respond_to do |format|
+      format.json { render :json => { :error => @error }, :status => :internal_server_error }
+    end
+  end
 end
